620c577bd3e760776df0e76870c7b4c2b2aac260
Tarun Sheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-10-18 21:05:56 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int marks[] {45,234,23,4};
+	//u can chamge value of array
+	marks[1]=877;
+	cout<<marks[0]<<endl;
+	cout<<marks[1]<<endl;
+	cout<<marks[2]<<endl;
+	cout<<marks[3]<<endl;
+	//u can chamge value of array
+	marks[1]=877;
+	
+	cout<<"These are math marks"<<endl;
+	int mathmarks[5];
+	mathmarks[0]=765;
+	mathmarks[1]=455;
+	mathmarks[2]=75;
+	mathmarks[3]=95;
+	mathmarks[4]=05;
+	cout<<mathmarks[0]<<endl;
+	cout<<mathmarks[1]<<endl;
+	cout<<mathmarks[2]<<endl;
+	cout<<mathmarks[3]<<endl;
+	cout<<mathmarks[4]<<endl;
+	
+	return 0;
+}
